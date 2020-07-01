2be83bbfeca7866a300d8bd8fe25bb4d82278d41
CarlosSchell <carlos.schellenberger@gmail.com>
2020-06-30 22:46:52 -0300

    lilian ramos

--- mongodb.ts
+++ mongodb.ts
@@ -1,10 +1,14 @@
 // 
 import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts"
 
-const MONGO_URL = `mongodb+srv://CarlosSchell:Chsmon1962@cluster0-osikw.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true` 
+const MONGO_URL = Deno.env.get(DB_HOST_URL) || 'mongodb://localhost:27017'
+//`mongodb+srv://CarlosSchell:Chsmon1962@cluster0-osikw.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true` 
+
 const client = new MongoClient()
 client.connectWithUri(MONGO_URL)
 
-const db = client.database('tjsp')
+const dbName = Deno.env.get(DB_NAME) || 'tjsp'
+
+const db = client.database(dbName)
 
 export default db
--- server.ts
+++ server.ts
@@ -1,6 +1,15 @@
-//
+/*jshint esversion: 6 */
+
 import { Application, Router } from "https://deno.land/x/oak/mod.ts"
 import {getPublicacoes, createPublicacao, getSinglePublicacao, updatePublicacao, deletePublicacao} from './routes.ts'
+
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+const env = config()
+
+console.log(env.DB_HOST_URL)
+console.log(env.DB_NAME)
+console.log(env.PORT)
 
 const router = new Router()
 
@@ -8,16 +17,20 @@ router
   .get('/', (ctx) => {
     ctx.response.body = 'Bem vindo a API do pesquisajus';
   })
-  .get('/api', getPublicacoes)
   .get('/api/:id', getSinglePublicacao)
-  .post('/api', createPublicacao)
-  .put('/api/:id', updatePublicacao)
-  .delete('/api/:id', deletePublicacao)
-  ;
-
+  //.get('/api', getPublicacoes)
+  //.post('/api', createPublicacao)
+  //.put('/api/:id', updatePublicacao)
+  //.delete('/api/:id', deletePublicacao)
+  
 const app = new Application()
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-app.listen({port: 8000})
-console.log('O Servidor está ativo e rodando na porta : ${port}' )
+const port = env.PORT || 8000
+
+//app.listen({port: port})
+//console.log('O Servidor está ativo e rodando na porta : ${port}' )
+
+console.log(`O Servidor está ativo e rodando na porta : ${port}`)
+await app.listen({port: +port});
