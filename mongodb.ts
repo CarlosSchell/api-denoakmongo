5a8315e4a123a8d2e2793d6e006afbb64c47645c
CarlosSchell <carlos.schellenberger@gmail.com>
2020-06-30 21:51:03 -0300

    ZECA BALEIRO

--- mongodb.ts
+++ mongodb.ts
@@ -1,7 +1,7 @@
 // 
 import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts"
 
-const MONGO_URL = `mongodb://localhost:27017` 
+const MONGO_URL = `mongodb+srv://CarlosSchell:Chsmon1962@cluster0-osikw.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true` 
 const client = new MongoClient()
 client.connectWithUri(MONGO_URL)
 
